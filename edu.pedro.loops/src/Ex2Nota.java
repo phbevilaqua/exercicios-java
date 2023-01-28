09cff6320cabcb3651eb97d7a0b631ab4c60bbec
phbevilaqua <pedrohenriquesb2001@hotmail.com>
2023-01-28 10:43:33 -0300

    Resolução de exercícios de loops

---
+++ edu.pedro.loops/src/Ex2Nota.java
@@ -0,0 +1,29 @@
+/*
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+*/
+
+import java.util.Scanner;
+
+public class Ex2Nota {
+    
+    public static void main(String[] args) {
+        int     nota;
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Digite uma nota entre 0 e 10: ");
+            nota = scan.nextInt();
+
+            if(nota < 0 || nota > 10){
+                System.out.println("Nota inválida.");
+            } else {
+                break;
+            }
+        }
+        System.out.println("A nota inserida foi: " + nota);
+    }
+
+}
---
+++ edu.pedro.loops/src/Ex3MaioreMedia.java
@@ -0,0 +1,18 @@
+/*
+Faça um programa que leia 5 números
+e informe o maior número
+e a média desses números.
+*/
+
+import java.util.Scanner;
+
+public class Ex3MaioreMedia {
+    
+    public static void main(String[] args){
+        int numeroAnterior, numeroAtual, soma;
+        soma = 0;
+
+        
+        
+    }
+}
